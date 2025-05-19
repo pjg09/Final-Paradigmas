3d38ff8119850cb89ef0ec839952d7b12d6e7784
Santiago Jaramillo <zyrocibe@gmail.com>
2025-05-19 11:57:24 -0500

    Cambios en Carro, creacion de electrico, gasolina, e hibrido

---
+++ BibliotecaFinalParadigmas/Clases/Electrico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFinalParadigmas.Clases
+{
+    internal class Electrico : Carro
+    {
+        //Atributos de control
+        private readonly short autonomiaMin = 200, autonomiaMax = 1000;
+
+        //Atributos de usuario
+        private ushort autonomia;
+
+
+        //Constructor
+        public Electrico(string placa, string marca, string modelo, ushort agno, Cliente duegnoCarro, byte autonomia) : base(placa, marca, modelo, agno, duegnoCarro)
+        {
+            Autonomia = autonomia;
+        }
+
+        //Accesor
+        //Verificamos que tenga entre 200km y 1000km de autonomia
+        public ushort Autonomia { get => autonomia;
+            set => Autonomia = value >= autonomiaMin && value <= autonomiaMax ? value : throw new Exception($"Error: El vehiculo debe tener entre {autonomiaMin}Km de autonomia minima y {autonomiaMax}Km de autonomia max");
+        }
+    }
+}
---
+++ BibliotecaFinalParadigmas/Clases/Hibrido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFinalParadigmas.Clases
+{
+    internal class Hibrido : Carro
+    {
+        //Atributos de control
+        private readonly byte numeroMinBaterias = 1, numeroMaxBaterias = 8;
+        //Atributos de usuario
+        private byte numeroBaterias;
+
+
+        //Constructor
+        public Hibrido(string placa, string marca, string modelo, ushort agno, Cliente duegnoCarro, byte numeroBaterias) : base(placa, marca, modelo, agno, duegnoCarro)
+        {
+            NumeroBaterias = numeroBaterias;
+        }
+        //Accesor para numeroBaterias
+        //Verificamos que el carro electrico tenga entre 1 y 8 baterias
+        public byte NumeroBaterias { get => numeroBaterias; 
+            set => numeroBaterias = value >= numeroMinBaterias && value <= numeroMaxBaterias ? value : throw new Exception($"Error: la cantidad de baterias debe estar entre {numeroMinBaterias} y {numeroMaxBaterias}");
+        }
+    }
+}
