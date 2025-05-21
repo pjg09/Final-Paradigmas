e61bec67e96a10513c78a7dcc6e9112c1da0eac1
Pedro Portatil <pedroj1229@gmail.com>
2025-05-21 12:33:25 -0500

    Holaaa

---
+++ Taller Web/Taller Web/Controllers/CarroController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taller_Web.Servicios;
+using BibliotecaFinalParadigmas.Clases;
+
+namespace Taller_Web.Controllers
+{
+    public class CarroController : Controller
+    {
+        private readonly CarroService _servicios;
+
+        public CarroController(CarroService servicio)
+        {
+            _servicios = servicio;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_servicios.ObtenerTodos());
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Crear(string placa, string)
+        {
+            var nuevo = new Carro()
+        }
+    }
+}
---
+++ Taller Web/Taller Web/Servicios/CarroService.cs
@@ -0,0 +1,19 @@
+﻿using BibliotecaFinalParadigmas.Clases;
+
+namespace Taller_Web.Servicios
+{
+    public class CarroService
+    {
+        private readonly List<Carro> lista_carros = new();
+
+        public void Agregar(Carro carro)
+        {
+            lista_carros.Add(carro);
+        }
+
+        public IEnumerable<Carro> ObtenerTodos()
+        {
+            return lista_carros;
+        }
+    }
+}
